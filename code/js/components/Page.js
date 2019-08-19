4be186f2efe5f96eb003f82dc8b983c7640c28d1
Sergio <sergiiy@tallium.com>
2019-08-20 00:05:39 +0300

    first commit

---
+++ code/js/components/Page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WebPage = () => (
+    <div>
+        <h1>Cars:</h1>
+        <hr/>
+        <h3>Details: </h3>
+    </div>
+);
+
+export default WebPage;
---
+++ code/js/reducers/car.js
@@ -0,0 +1,25 @@
+export default function () {
+    return [
+        {
+            id: 1,
+            model: "Audi",
+            speed: 230,
+            weight: 1.2,
+            desc: "R8 the best auto",
+        },
+        {
+            id: 2,
+            model: "BMW",
+            speed: 200,
+            weight: 1.1,
+            desc: "BMW the best auto",
+        },
+        {
+            id: 3,
+            model: "Mercedes",
+            speed: 150,
+            weight: 1,
+            desc: "Mercedes the best auto",
+        },
+    ];
+}
